d60e3f67f5c9bf131da7301a6772b603790f19e4
modeal-chan <chanchan2@1-mm.net>
2020-09-02 17:42:20 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Coin from './Coin';
+
+
 
 function App() {
+  const [ coins, setCoins ] = useState([]);
+  const [ search, setSearch ] = useState('');
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+  // fillter coins and display whatever user type in
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data); // res: response
+    })
+    .catch(error => console.log(error))
+  }, []);
+
   return (
-    <div className="App">
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input 
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return(
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        )
+      })}
     </div>
   );
 }
